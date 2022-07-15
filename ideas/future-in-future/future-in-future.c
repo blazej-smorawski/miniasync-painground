a24483cbabdf1837bce5b3237971b183d056da39
blazej-smorawski <blazej.smorawski@gmail.com>
2022-07-15 10:41:10 +0200

    future-in-future updates

--- ideas/future-in-future/future-in-future.c
+++ ideas/future-in-future/future-in-future.c
@@ -13,6 +13,7 @@
 struct timer_future_data {
     double time;
     struct timespec start_time;
+    pthread_mutex_t lock;
 };
 
 struct timer_future_output {
@@ -22,13 +23,17 @@ struct timer_future_output {
 FUTURE(timer_future, struct timer_future_data, struct timer_future_output);
 
 static enum future_state
-timer_future_implementation(struct future_context *ctx, struct future_notifier *notifier)
+timer_future_implementation(struct future_context *ctx,
+	struct future_notifier *notifier)
 {
 	struct timer_future_data *data = future_context_get_data(ctx);
 	struct timespec now_time;
 
+	/*
+	 * Probably unsafe without a mutex
+	 */
 	clock_gettime(CLOCK_REALTIME, &now_time);
-	if (timediff(&now_time,&data->start_time) >= data->time) {
+	if (timediff(&now_time, &data->start_time) >= data->time) {
 		printf("Timer done\n");
 		return FUTURE_STATE_COMPLETE;
 	} else {
@@ -44,7 +49,8 @@ timer(int time)
 	struct timer_future future = {
 		.output.unused = 0,
 		.data.time = time,
-		};
+	};
+	pthread_mutex_init(&future.data.lock, NULL);
 	clock_gettime(CLOCK_REALTIME, &future.data.start_time);
 
 	FUTURE_INIT(&future, timer_future_implementation);
@@ -70,7 +76,8 @@ struct caller_future_output {
 FUTURE(caller_future, struct caller_future_data, struct caller_future_output);
 
 static enum future_state
-caller_future_implementation(struct future_context *ctx, struct future_notifier *notifier)
+caller_future_implementation(struct future_context *ctx,
+	struct future_notifier *notifier)
 {
 	struct caller_future_data *data = future_context_get_data(ctx);
 	struct caller_future_output *output = future_context_get_output(ctx);
@@ -81,7 +88,7 @@ caller_future_implementation(struct future_context *ctx, struct future_notifier 
 	 * Static future should be 0'ed, so if the first 64 bits are 0 we
 	 * can guess it wasn't initialized yet
 	 */
-	if(*((long long *)(&static_timer)) == 0) {
+	if (*((long long *)(&static_timer)) == 0) {
 		static_timer = timer(5);
 		return FUTURE_STATE_RUNNING;
 	} else {
@@ -108,12 +115,32 @@ caller(int number)
 
 	return future;
 }
+
 /*
  * ----==================----
  */
 
-int main(void) {
-	struct runtime *r = runtime_new();
+void *worker(void *arg)
+{
+	struct caller_future fut = caller((int)arg);
+	FUTURE_BUSY_POLL(&fut);
+	return NULL;
+}
+
+int main(void)
+{
+	int n = 4;
+	pthread_t threads[n];
+
+	for (long i = 0; i < n; i++) {
+		pthread_create(&threads[i], NULL, worker, (void *)i);
+	}
+
+	for (long i = 0; i < n; i++) {
+		pthread_join(threads[i], NULL);
+	}
+
+	/*struct runtime *r = runtime_new();
 	struct caller_future callers[] = {
 		caller(1),
 		caller(2),
@@ -128,6 +155,6 @@ int main(void) {
 		FUTURE_AS_RUNNABLE(&callers[3]),
 	};
 
-	runtime_wait_multiple(r, runnables,4);
+	runtime_wait_multiple(r, runnables,4);*/
 	return 0;
 }
